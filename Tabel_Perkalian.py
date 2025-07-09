12edc4d24be5550c48072a57f31c24e430455760
Goodwillhakim <goodwillhakim9@gmail.com>
2025-07-09 21:47:25 +0700

    NEW

---
+++ Tabel_Perkalian.py
@@ -0,0 +1,8 @@
+print("=" *41)
+print("             Tabel Perkalian")
+print("=" *41)
+
+for angka in range (1, 11) :
+    for kelipatan in range (1, 11) :
+        print(f" {angka} x {kelipatan} = {angka * kelipatan} ")
+    
